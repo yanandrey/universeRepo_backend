03e24cd4a66ddea125a5bd30cf206e51c14fde6f
Yan Andrey <43590571+yanandrey@users.noreply.github.com>
2023-11-26 02:33:23 -0300

    alteração para banco de dados em memória

--- UniverseRepo/Configurations/DbContextServiceCollection.cs
+++ UniverseRepo/Configurations/DbContextServiceCollection.cs
@@ -7,16 +7,16 @@ public static class DbContextServiceCollection
 {
     public static void AddDbContextServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration["ConnectionStrings:DatabaseConnection"];
+        //var connectionString = configuration["ConnectionStrings:DatabaseConnection"];
 
         services.AddDbContext<ApiDbContext>(options =>
         {
-            options.UseSqlServer(connectionString, x =>
-            {
-                x.EnableRetryOnFailure(1, TimeSpan.FromSeconds(1), null);
-            });
+            //options.UseSqlServer(connectionString, x =>
+            //{
+            //    x.EnableRetryOnFailure(1, TimeSpan.FromSeconds(1), null);
+            //});
 
-            //options.UseInMemoryDatabase("UniverseRepo");
+            options.UseInMemoryDatabase("UniverseRepo");
         });
     }
 }
